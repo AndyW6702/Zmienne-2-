1a37a1570f23ae8d251ba649e9253eae34ba1621
AndyW6702 <andrzejwilczy@op.pl>
2024-02-05 20:58:26 +0100

    Dodaj pliki projektów.

---
+++ Zmienne 2/Program.cs
@@ -0,0 +1,21 @@
+﻿bool isWomen = true;
+int age = 18;
+string name = "Ewa";
+
+if (isWomen && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (!isWomen && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Inna osoba");
+}
+
